e9c0a5016313bb9f3958871d38c4dfc5a40f377e
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-03-18 13:59:10 +0530

    configured the mongo db connection.

---
+++ Backend/src/config/mongo.connection.ts
@@ -0,0 +1,34 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import { AppError } from "../Error/appError";
+
+let client: any;
+let db: any;
+
+const connectDB = async () => {
+  if (db) return db; // reuse existing connection
+
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new AppError(
+        "MONGODB_URI is not defined in environment variables",
+        500
+      )
+    }
+
+    client = new MongoClient(process.env.MONGODB_URI);
+
+    await client.connect();  // it will establish the connection 
+
+    db = client.db(); // default DB from URI
+
+    console.log("MongoDB is Connected Successfully");
+
+    return db;
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1); // fail fast
+  }
+};
+
+export default connectDB;
