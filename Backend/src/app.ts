4131d52f76470a42e4bf76aeadaee3d94eb07365
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-03-23 11:10:18 +0530

    connect redis and middlewares

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,27 @@
 import express, { Request, Response } from "express";
+import passport from "passport";
 import connectDB from "./config/mongo.connection";
+import "./config/passport.config"; // Initialize passport configuration
 import { errorHandler } from "./middlewares/errorHandling.middleware";
+import userRouter from "./modules/user/user.routes";
+import { redisConnection } from "./config/redis.connection";
 
 // Create a new express application instance
 const app = express();
 
 // connect the mongoDB database
 connectDB();
+
+// connect the redis client here
+redisConnection();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());  // initialize the passport middleware
+
+// Routes
+app.use("/auth", userRouter);
 
 // Set the network port
 const port = process.env.PORT || 4000;
